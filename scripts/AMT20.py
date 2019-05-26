d33f380e3faa9b3094831be58dbb86b379759440
Adrian Carreño <adriancarreno.d@gmail.com>
2019-05-26 11:26:00 -0400

    Function files separated from main script

---
+++ scripts/AMT20.py
@@ -0,0 +1,34 @@
+# You must 'import spidev' in your script when calling this function
+# and create a SPI object with
+# enc = spidev.SpiDev()
+
+
+nop_a5 = 0x00
+rd_pos = 0x10
+set_zero_point = 0x70
+
+
+# This command causes a read of the current position.
+def read_position(enc):
+    resp = enc.xfer([rd_pos], 0, 20)
+
+    while resp[0] != rd_pos:
+        resp = enc.xfer([nop_a5], 0, 20)
+
+    MSB = enc.xfer([nop_a5], 0, 20)[0]  # Most Significant Byte
+    LSB = enc.xfer([nop_a5], 0, 20)[0]  # Least Significant Byte
+    return (MSB << 8) | LSB
+
+
+# This command sets the current position to zero and saves this setting in the EEPROM.
+def set_zero(enc):
+    resp = enc.xfer([set_zero_point], 0, 20)
+
+    while resp[0] != 0x80:
+        resp = enc.xfer([nop_a5], 0, 20)
+
+    # The encoder must be power cycled. If the encoder is not power cycled, the position
+    # values will not be calculated off the latest zero position. When the encoder is
+    # powered on next the new offset will be used for the position calculation.
+
+    return True
--- scripts/spi_reader.py
+++ scripts/spi_reader.py
@@ -4,10 +4,7 @@ import rospy
 from scooter.msg import spi
 import spidev
 import time
-
-nop_a5 = 0x00
-rd_pos = 0x10
-set_zero_point = 0x70
+from AMT20 import read_position
 
 enc1 = spidev.SpiDev()
 enc1.open(0, 0)
@@ -20,36 +17,6 @@ enc2.max_speed_hz = 976000
 data = spi()
 
 
-# This command causes a read of the current position.
-
-def read_position(enc):
-    resp = enc.xfer([rd_pos])
-
-    while resp[0] != rd_pos:
-        resp = enc.xfer([nop_a5], 0, 20)
-
-    MSB = enc.xfer([nop_a5], 0, 20)[0] # Most Significant Byte
-    LSB = enc.xfer([nop_a5], 0, 20)[0] # Least Significant Byte
-    return (MSB << 8) | LSB
-
-
-# This command sets the current position to zero and saves this setting in the EEPROM.
-
-def set_zero(enc):
-    enc.writebytes([set_zero])
-    response = enc.readbytes(1)
-
-    while (response != 0x80):
-        enc.writebytes([nop_a5])
-        response = enc.readbytes(1)
-
-    # The encoder must be power cycled. If the encoder is not power cycled, the position
-    # values will not be calculated off the latest zero position. When the encoder is
-    # powered on next the new offset will be used for the position calculation.
-
-    return
-
-
 def talker():
     pub = rospy.Publisher('encoders', spi, queue_size=10)
     rospy.init_node('encoder_reader', anonymous=True)
@@ -59,8 +26,7 @@ def talker():
         data.enc1 = read_position(enc1)
         data.enc2 = read_position(enc2)
         pub.publish(data)
-        rospy.loginfo('Encoder 1: %s, Encoder 2: %s', str(data.enc1),
-                      str(data.enc2))
+        rospy.loginfo('Enc1: %s, Enc2: %s', str(data.enc1), str(data.enc2))
         rate.sleep()
 
 
